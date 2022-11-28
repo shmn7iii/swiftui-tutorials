59c89ccc7fc5a11bce8ba2409c59cb02b2904cb4
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-11-28 16:32:03 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by shmn7iii on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
