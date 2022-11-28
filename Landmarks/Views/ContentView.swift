434f9dfe1fa31e53bfefd862f3dc2650cb1d87ff
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-11-28 18:38:17 +0900

    section2: Building Lists and Navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by shmn7iii on 2022/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
